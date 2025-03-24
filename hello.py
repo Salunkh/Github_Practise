8e9a5085032c6bc2a240b38b2bb48028a96bee43
Salunkh <88866444+Salunkh@users.noreply.github.com>
2025-03-25 00:47:01 +0530

    my 4th changes

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 print("Hello, World!")
 print("Hello, Muttal")
 
+
 def fibonacci(n):
     # Initialize the first two numbers in the Fibonacci series
     fib_series = [0, 1]
@@ -15,7 +16,7 @@ def fibonacci(n):
 # Example: Get the first 10 numbers in the Fibonacci series
 n = 10
 fib_sequence = fibonacci(n)
-
+print("hello mmutal anup")
 # Print the Fibonacci series
 print(f"The first {n} numbers in the Fibonacci series are:")
 print(fib_sequence)
